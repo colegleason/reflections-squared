b87e49edccc3f6adae7cd5b428bda8cf9969c440
Ciara Proctor <ciarasproctor@gmail.com>
2013-04-29 18:56:58 -0500

    attempt at year list

--- js/reflections.js
+++ js/reflections.js
@@ -1,10 +1,28 @@
 function load_speakers() {
-    d3.json("speakers.json", function(error, json) {
+    d3.json("data.json", function(error, json) {
         if (error) return console.warn(error);
         var data = json;
         var content = d3.select("#content");
         top_affiliations(content, data.years, 5);
 });
+}
+
+function load_data() {
+	d3.json("data.json", function(error,json)
+		if (error) return console.warn(error);
+		var data = json;
+		var content = d3.select("#years");
+		year_list(content, data.years)
+);
+}
+
+function year_list(root, data) {
+	var graph = root.append("div").append("ol");
+	graph.selectAll("li")
+		.data(data)
+		.enter()
+		.append("li")
+		.text(function(d) {return d});
 }
 
 function collapse_speakers(years) {
